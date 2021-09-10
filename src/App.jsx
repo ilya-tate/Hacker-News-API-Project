518276d74feb8beb0710f88818771b9ce493d84d
Ilya Tate <ilyatate@gmail.com>
2021-09-10 15:28:36 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from './Components/Home';
+
+export const App = () => {
+  return (
+    <Home />
+  );
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Home = () => {
+  return (
+    <div>
+      Really cool stuff is gonna go here and be super epic like I am
+    </div>
+  );
+}
